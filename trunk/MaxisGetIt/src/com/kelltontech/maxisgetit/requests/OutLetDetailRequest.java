c81020ca11f6552a3f659cffac3c62c5022acf27
manish.verma <manish.verma@dc5b7a07-1871-418f-b689-a03735bf73cc>
2014-01-17 07:02:09 +0000

    request data

---
+++ trunk/MaxisGetIt/src/com/kelltontech/maxisgetit/requests/OutLetDetailRequest.java
@@ -0,0 +1,24 @@
+package com.kelltontech.maxisgetit.requests;
+
+public class OutLetDetailRequest {
+
+	private String deal_id;
+	private String comp_id;
+
+	public String getDeal_id() {
+		return deal_id;
+	}
+
+	public void setDeal_id(String deal_id) {
+		this.deal_id = deal_id;
+	}
+
+	public String getComp_id() {
+		return comp_id;
+	}
+
+	public void setComp_id(String comp_id) {
+		this.comp_id = comp_id;
+	}
+	
+}
